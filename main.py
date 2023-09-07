5a4b2570b4975ff59bdede09e3ac8ae7eca6543e
EBusch1 <elijah.t.busch@gmail.com>
2023-09-06 21:49:48 -0500

    Updated lists.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import string
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
@@ -9,10 +10,9 @@ import pyperclip
 def generate_password():
     password_entry.delete(0, END)
 
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-               'v', 'w', 'x', 'y', 'z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    letters = string.ascii_letters
+    numbers = string.digits
+    symbols = string.punctuation
 
     pwd_letters = [choice(letters) for _ in range(randint(8, 10))]
     pwd_numbers = [choice(numbers) for _ in range(randint(2, 4))]
